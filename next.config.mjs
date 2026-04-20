a476c7b051cf5195dbe35bbab999fddc1d1d8e0e
Ruth <99640531+moraaruth@users.noreply.github.com>
2026-04-20 06:16:24 +0300

    Add output configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Add this line
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'plus.unsplash.com' },
     ],
-    unoptimized: true,
+    unoptimized: true, 
   },
 };
 
